4de3262341e9762a2f33f83bc6b178e72d25eab2
Sudharshan <sudhar393@gmail.com>
2018-06-20 09:43:38 +0800

    added basic motor control

---
+++ zumoros_client/zumoros_client.ino
@@ -0,0 +1,46 @@
+#define USBCON
+
+#include <ros.h>
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+#include <std_msgs/Int16.h>
+
+// Function Prototypes
+void lmotorCb(const std_msgs::Int16 &lmotor_msg);
+void rmotorCb(const std_msgs::Int16 &rmotor_msg);
+
+// Zumo Object Initializations
+Zumo32U4Motors motors;
+
+// ROS Initializations
+ros::NodeHandle nh;
+ros::Subscriber<std_msgs::Int16> lmotor_sub("lmotor", &lmotorCb);
+ros::Subscriber<std_msgs::Int16> rmotor_sub("rmotor", &rmotorCb);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  nh.initNode();
+  nh.subscribe(lmotor_sub);
+  nh.subscribe(rmotor_sub);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  nh.spinOnce();
+  delay(1);
+}
+
+void lmotorCb(const std_msgs::Int16 &lmotor_msg) {
+  // Do smth in the callback
+  int16_t leftSpeed = constrain(lmotor_msg.data, -400, 400);
+  motors.setLeftSpeed(leftSpeed);
+}
+
+void rmotorCb(const std_msgs::Int16 &rmotor_msg) {
+  // Do smth in the callback
+  int16_t rightSpeed = constrain(rmotor_msg.data, -400, 400);
+  motors.setRightSpeed(rightSpeed);
+}
+
